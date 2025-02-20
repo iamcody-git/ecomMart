cb4d77005c86a273ba70df11ace57d5f8a069375
iamcody-git <iamcody.github@gmail.com>
2025-02-20 13:42:19 +0545

    first commit

---
+++ config/dbConfig.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose
+console.log("connected")
+
+if(!cached){
+    cached = global.mongoose = {conn:null, Promise:null}
+
+
+}
+
+async function connectDB() {
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.Promise){
+        const opts = {
+            bufferCommands:false
+        }
+
+        cached.Promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose=> {return mongoose})
+    }
+    
+    cached.conn = await cached.Promise
+    return cached.conn
+}
+
+export default connectDB
